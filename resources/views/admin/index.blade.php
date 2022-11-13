2a5ac5e87abeea399e6abeac89c86c3acd90d53c
Faizalqossam <alqassamf@gmail.com>
2022-11-13 12:52:02 +0700

    refactoring views & link

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,93 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-<meta charset="utf-8">
-<meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-<title>Dashboard - Admin</title>
-<meta content="" name="description">
-<meta content="" name="keywords">
-
-<!-- Favicons -->
-<link href="{{ url('/public/admin/img/favicon.png') }}" rel="icon">
-<link href="{{ url('/public/admin/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
-
-<!-- Google Fonts -->
-<link href="https://fonts.gstatic.com" rel="preconnect">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-<!-- Vendor CSS Files -->
-<link href="{{ asset('/public/admin/vendor/bootstrap/css/bootstrap.min.css') }} " rel="stylesheet">
-<link href="{{ asset('/public/admin/vendor/bootstrap-icons/bootstrap-icons.css') }} " rel="stylesheet">
-<link href="{{ asset('/public/admin/vendor/boxicons/css/boxicons.min.css') }} " rel="stylesheet">
-<link href="{{ asset('/public/admin/vendor/quill/quill.snow.css') }} " rel="stylesheet">
-<link href="{{ asset('/public/admin/vendor/quill/quill.bubble.css') }} " rel="stylesheet">
-<link href="{{ asset('/public/admin/vendor/remixicon/remixicon.css') }} " rel="stylesheet">
-<link href="{{ asset('/public/admin/vendor/simple-datatables/style.css') }} " rel="stylesheet">
-
-<!-- Template Main CSS File -->
-<link href="{{ asset('/public/admin/css/style.css') }} " rel="stylesheet">
-
-<!-- =======================================================
-* Template Name: NiceAdmin - v2.4.1
-* Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-======================================================== -->
-</head>
-
-<body>
 
 <!-- ======= Header ======= -->
-@include('admin.header')
+@include('admin.layout.header')
 <!-- End Header -->
 
 <!-- ======= Sidebar ======= -->
-@include('admin.sidebar')
+@include('admin.layout.sidebar')
 <!-- End Sidebar-->
 
 <main id="main" class="main">
 
-<div class="pagetitle">
-    <h1>Dashboard</h1>
-    <nav>
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-        <li class="breadcrumb-item active">Dashboard</li>
-    </ol>
-    </nav>
-</div><!-- End Page Title -->
 
-<section class="section dashboard">
-    <div class="row">
+{{-- @foreach ($products as $product)
+    <p class="fw-bold"> {{ $product->name }} </p>
+    <p class="fw-bold"> {{ $product->code }} </p>
+    <p class="fw-bold"> Rp.{{ number_format( $product->price )}} </p>
+    <p class="fw-bold"> {{ $product->stok }} </p>
+    <p class="fw-bold"> {{ $product->sold }} </p>
+    <p class="fw-bold"> {{ $product->category->name }} </p>
+    <p class="fw-bold"> {{ $product->store->name }} </p>
+    <p class="fw-bold"> {{ $product->store->location }} </p>
+    <p class="fw-bold"> {{ $product->store->rating }} </p>
+@endforeach --}}
 
-        @yield('content')
-
-    </div>
-</section>
+@yield('content')
 
 </main><!-- End #main -->
 
 <!-- ======= Footer ======= -->
-@include('admin.footer')
+@include('admin.layout.footer')
 <!-- End Footer -->
 
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-<!-- Vendor JS Files -->
-<script src="{{ asset('/public/admin/vendor/apexcharts/apexcharts.min.js') }} "></script>
-<script src="{{ asset('/public/admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }} "></script>
-<script src="{{ asset('/public/admin/vendor/chart.js/chart.min.js') }} "></script>
-<script src="{{ asset('/public/admin/vendor/echarts/echarts.min.js') }} "></script>
-<script src="{{ asset('/public/admin/vendor/quill/quill.min.js') }} "></script>
-<script src="{{ asset('/public/admin/vendor/simple-datatables/simple-datatables.js') }} "></script>
-<script src="{{ asset('/public/admin/vendor/tinymce/tinymce.min.js') }} "></script>
-<script src="{{ asset('/public/admin/vendor/php-email-form/validate.js') }} "></script>
-
-<!-- Template Main JS File -->
-<script src="{{ asset('/public/admin/js/main.js') }} "></script>
-
-</body>
-
-</html>
