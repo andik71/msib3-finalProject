296ec85ca4f236b817e452cc0482cc683f4f0394
Faizalqossam <alqassamf@gmail.com>
2022-11-13 12:52:56 +0700

    routing products, category & store

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('landingpage.home');
+// Route::get('/administrator', [ProductController::class, 'index']);
+
+Route::resource('admin/product', ProductController::class);
+Route::resource('admin/category', CategoryController::class);
+Route::resource('admin/store', StoreController::class);
+
+Route::get('/admin/detailproduct', function () {
+    return view('admin.detailproduct');
 });
 
-//routing admin dashboard
-Route::get('/administrator', function () {
+Route::get('/admin', function () {
     return view('admin.home');
+
 });
+
+// //routing admin dashboard
+// Route::get('/administrator', function () {
+//     return view('admin.home');
+// });
 
 //routing landingpage
 Route::get('/about', function () {
