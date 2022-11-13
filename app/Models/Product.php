daf6a5b7f245ce46220d5cc4d336a20da8ac370d
Faizalqossam <alqassamf@gmail.com>
2022-11-13 12:58:00 +0700

    config relational database

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+
+    protected $fillable = ['name','code','price','stok','sold','photo'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function store(){
+        return $this->belongsTo(Store::class);
+    }
+
+    public function orderDetails(){
+        return $this->hasMany(OrderDetails::class);
+    }
+}
