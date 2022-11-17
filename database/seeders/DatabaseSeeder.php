14271d2b12334dc4e7793634f87c41d3bfde538c
Faizalqossam <alqassamf@gmail.com>
2022-11-17 20:01:32 +0700

    adding new collumn products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,10 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Category;
+use App\Models\OrderDetails;
 use App\Models\Product;
 use App\Models\Store;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -28,34 +30,34 @@ class DatabaseSeeder extends Seeder
         $products = [
             [
                 'code' => 'SM01',
-                'name' => 'Iphone 13',
-                'desc' => 'Iphone 2021',
-                'price' => '3000000',
+                'name' => 'Apple Iphone 14 Pro',
+                'desc' => 'Apple iPhone 14 Pro Processor : Apple A16 Bionic (4 nm) Hexa-core (2x3.46 GHz Everest + 4x2.02 GHz Sawtooth) Memory : RAM 6GB + ROM 128GB Display : 6.1 inches 1179 x 2556 pixels, 19.5:9 ratio (~460 ppi density) LTPO Super Retina XDR OLED, 120Hz, HDR10, Dolby Vision, 1000 nits (typ), 2000 nits (HBM) Battery : Li-Ion 3200 mAh Network : GSM / CDMA / HSPA / EVDO / LTE / 5G OS : iOS 16',
+                'price' => '20000000',
                 'stok' => '100',
                 'sold' => '50',
-                'photo' => '',
+                'photo' => 'iphone14-1.jpg',
                 'category_id' => '1',
                 'store_id' => '1',
             ],
             [
                 'code' => 'LP01',
-                'name' => 'Asus Rog Strix',
-                'desc' => 'Asus Rog 2022',
-                'price' => '50000000',
+                'name' => 'ASUS ROG STRIX SCAR 17 SE G733CX',
+                'desc' => 'Processor : Intel® Core™ i9-12950HX Processor 2.3 GHz (30M Cache, up to 5.0 GHz, 16 cores: 8 P-cores and 8 E-cores) Memory : 32GB (16GB DDR5-4800 SO-DIMM *2 Graphics : NVIDIA® GeForce RTX™ 3080 Ti Laptop GPU 8GB GDDR6 MUX Switch + Optimus Storage : 2TB M.2 NVMe™ PCIe® 4.0 SSD Display : 17.3-inch WQHD (2560 x 1440) 16:9 IPS-level ; Refresh Rate 240Hz ; Response Time (G2G) 3ms ; Brightness 300nits ; DCI-P3 % 100% OS : Windows 11 Home Single Language 64',
+                'price' => '10439000',
                 'stok' => '20',
                 'sold' => '10',
-                'photo' => '',
+                'photo1' => 'asusrogstrix-1.jpg',
                 'category_id' => '2',
                 'store_id' => '2',
             ],
             [
                 'code' => 'PC01',
-                'name' => 'HP Gl 05',
-                'desc' => 'HP PC All In One',
-                'price' => '10000000',
+                'name' => 'Apple IMac M1 24”',
+                'desc' => 'Processor : Apple M1 chip, CPU 8-core, Neural Engine 16-core Memory : Integrated memory 8GB Graphics : GPU up to 8-core Storage : SSD 256GB Display : Retina 4.5K, 24 inci, resolusi 4480 x 2520 piksel, 500 nit OS : macOS 10.13 High Sierra',
+                'price' => '25125000',
                 'stok' => '120',
                 'sold' => '30',
-                'photo' => '',
+                'photo' => 'appleimac-1.jpg',
                 'category_id' => '3',
                 'store_id' => '3',
             ],
@@ -64,38 +66,59 @@ class DatabaseSeeder extends Seeder
         $category = [
             [
                 'name' => 'Smartphone',
-                'photo' => ''
+                'photo' => 'smartphone.jpg'
             ],
             [
                 'name' => 'Laptop',
-                'photo' => ''
+                'photo' => 'laptop.jpg'
             ],
             [
                 'name' => 'PC All In One',
-                'photo' => ''
+                'photo' => 'pc_all_in_one.png'
             ]
         ];
 
         $store = [
             [
-                'name' => 'pstore',
-                'location' => 'jakarta',
+                'name' => 'Pstore',
+                'location' => 'Jakarta',
                 'rating' => '4.5'
             ],
             [
-                'name' => 'studio ponsel',
-                'location' => 'depok',
+                'name' => 'Studio Ponsel',
+                'location' => 'Depok',
                 'rating' => '4.9'
             ],
             [
-                'name' => 'wah gadget',
-                'location' => 'bekasi',
+                'name' => 'Wah Gadget',
+                'location' => 'Bekasi',
                 'rating' => '4.7'
             ],
         ];
 
+        // $user = [
+        //     [
+        //         'name' => 'Faiz',
+        //         'email' => 'alqassamf@gmail.com',
+        //         'password' => 'alqassamf123',
+        //         'address' => 'Bekasi',
+        //         'phone_number' => '089698085698',
+        //     ],
+        // ];
+        
+        // $order_details = [
+        //     [
+        //         'total_order' => '2',
+        //         'total_price' => '40000000',
+        //         'products_id' => '1',
+        //         'user_id' => '1',
+        //     ],
+        // ];
+
         Category::insert($category);
         Store::insert($store);
         Product::insert($products);
+        // User::insert($user);
+        // OrderDetails::insert($$order_details);
     }
 }
